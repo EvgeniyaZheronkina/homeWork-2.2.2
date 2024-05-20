c11fbf15ca3c1ee49b98be36fb807db6a641e22a
Евгения <zheronkina86@gmail.com>
2024-05-21 00:49:30 +0300

    All project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Bicycle bicycle = new Bicycle("bmv", 2);
+        Car car = new Car("bmv", 4);
+        Truck truck = new Truck("truck", 6);
 
+        ServiceStation station = new ServiceStation();
+        station.check(bicycle, car, truck);
     }
+
 }
